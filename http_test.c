e651f8feb4e073d65756c021cf4d8305b70fe8c5
xb-hub <934814993@qq.com>
2021-10-16 23:05:50 +0800

    add static http

--- http_test.c
+++ http_test.c
@@ -4,19 +4,25 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main(int argc, char *argv)
 {
+    if(argc < 2)
+    {
+        printf("Usage: parameter port\n");
+        return -1;
+    }
     int clientfd;
     struct sockaddr_in addr;
     char buf[1024];
     char response[1024];
+    u_short port = (u_short)atoi(argv[1]);
     if((clientfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
     {
         printf("socket creat fail!");
         return -1;
     }
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(4000);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     if(connect(clientfd, (struct scokaddr *)&addr, sizeof(addr)) == -1)
     {
